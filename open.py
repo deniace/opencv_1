95357ee8f440b0b79b5080e9a8a0b901c3070c65
Deni Supriyatna <denisupriyatna01@gmail.com>
2026-02-07 17:01:12 +0700

    update

--- open.py
+++ open.py
@@ -35,3 +35,42 @@ if not os.path.exists(asset_zip_path):
 
 # display 18x18 pixel image
 Image(filename="checkerboard_18x18.png")
+
+# display 84x84 pixel image
+Image(filename="checkerboard_84x84.jpg")
+
+# read image as gray scale
+cb_img = cv2.imread("checkerboard_18x18.png",0)
+
+# print the image data (pixel value), element of a 2D numpy array
+# each pixel value is 8bit [0,255]
+print(cb_img)
+
+# print the size of image
+print("Image size (H, W) is : ", cb_img.shape)
+
+# print data type of image
+print("Data type image is : ", cb_img.dtype)
+
+# Display image using matplotlib
+plt.imshow(cb_img)
+# plt.show()
+
+# set color map to gray scale for proper rendering
+plt.imshow(cb_img, cmap="gray")
+# plt.show()
+
+
+# read image as gray scale
+cb_img_fuzzy = cv2.imread("checkerboard_fuzzy_18x18.jpg", 0)
+
+# print image
+print(cb_img_fuzzy)
+
+# display image
+plt.imshow(cb_img_fuzzy, cmap="gray")
+# plt.show()
+
+print("last")
+# read and display coca-cola logo
+Image(filename="coca-cola-logo.png") # ini masih ga ke display image nya
